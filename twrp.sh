ee9685a55ee593ec7c261f2c093be03d6861a41f
Aghora7 <ukikieran@gmail.com>
2020-12-11 22:18:37 +0530

    Add files via upload

---
+++ twrp.sh
@@ -0,0 +1,4 @@
+adb reboot bootloader
+fastboot flash recovery '/home/aghora7/Videos/GSI/red8a.img' 
+fastboot flash vbmeta '/home/aghora7/Videos/GSI/vbmeta3.img' 
+fastboot boot '/home/aghora7/Videos/GSI/red8a.img'
