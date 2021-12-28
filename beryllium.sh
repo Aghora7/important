6e599e4220692b2dec76238fc7acb5d8260385e7
Aghora7 <ukikieran@gmail.com>
2021-12-29 04:28:11 +0530

    Create beryllium.sh

---
+++ beryllium.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/XtremeOrnob/xiaomi_beryllium -b aosp-12 device/xiaomi/beryllium
+git clone https://github.com/XtremeOrnob/xiaomi_sdm845-common -b aosp-12 device/xiaomi/sdm845-common
+git clone https://github.com/XtremeOrnob/XDKernel kernel/xiaomi/sdm845
+git clone https://github.com/XtremeOrnob/vendor_xiaomi.git -b aosp-12 vendor/xiaomi
+git clone https://github.com/LineageOS/android_hardware_qcom_display hardware/qcom/display
+git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
+git clone https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom/media
+git clone https://github.com/lineageos/android\_packages\_resources\_devicesettings packages/resources/devicesettings
