f6d54df982ae9a70fd86b8d4883f44a0ce8c391c
Aghora7 <ukikieran@gmail.com>
2022-03-24 14:49:11 +0530

    update it

--- beryllium.sh
+++ beryllium.sh
@@ -1,12 +1,6 @@
-git clone https://github.com/aghora7/device_xiaomi_beryllium -b twelve device/xiaomi/beryllium
-git clone https://github.com/XtremeOrnob/xiaomi_sdm845-common -b aosp-12 device/xiaomi/sdm845-common
-git clone https://github.com/XtremeOrnob/XDKernel kernel/xiaomi/sdm845
-git clone https://github.com/XtremeOrnob/vendor_xiaomi.git -b aosp-12 vendor/xiaomi
-git clone https://github.com/LineageOS/android_hardware_qcom_display hardware/qcom/display
-git clone https://github.com/LineageOS/android_hardware_qcom_audio hardware/qcom/audio
-git clone https://github.com/LineageOS/android_hardware_qcom_media hardware/qcom/media
-sudo rm -r hardware/qcom-caf/sdm845/audio
-sudo rm -r hardware/qcom-caf/sdm845/media
-sudo rm -r hardware/qcom-caf/sdm845/display
+git clone https://github.com/aghora7/device_xiaomi_beryllium device/xiaomi/beryllium
+git clone https://github.com/Aghora7/device_xiaomi_sdm845-common device/xiaomi/sdm845-common
+git clone https://github.com/PainKiller3/kernel_xiaomi_sdm845 kernel/xiaomi/sdm845
+git clone https://github.com/PainKiller3/vendor_xiaomi vendor/xiaomi
 git clone https://github.com/lineageos/android\_packages\_resources\_devicesettings packages/resources/devicesettings
-git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-custom --depth=1
