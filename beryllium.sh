2e9180c98191434488ade48693b8d079264309a4
Aghora7 <ukikieran@gmail.com>
2022-09-26 04:10:33 +0530

    Update beryllium.sh

--- beryllium.sh
+++ beryllium.sh
@@ -1,7 +1,7 @@
-git clone https://github.com/RahulGorai0206/device_xiaomi_beryllium -b thirteen device/xiaomi/beryllium
+git clone https://github.com/RahulGorai0206/device_xiaomi_beryllium -b 13-main device/xiaomi/beryllium
 git clone https://github.com/RahulGorai0206/device_xiaomi_sdm845-common.git -b thirteen device/xiaomi/sdm845-common
 git clone https://github.com/RahulGorai0206/vendor_xiaomi -b thirteen vendor/xiaomi
 git clone https://github.com/PainKiller3/kernel_xiaomi_sdm845.git -b thirteen kernel/xiaomi/sdm845
 git clone https://gitlab.com/RahulGorai0206/firmware_xiaomi_beryllium -b 13 firmware/xiaomi/beryllium
-git clone https://github.com/lineageos/android\_packages\_resources\_devicesettings packages/resources/devicesettings
-git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-custom --depth=1
+git clone https://github.com/lineageos/android_packages_resources_devicesettings -b lineage-20.0 packages/resources/devicesettings
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
