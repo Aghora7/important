4f84dae91dd5ceb258a286e25d3023e2ed387c99
Aghora7 <ukikieran@gmail.com>
2022-09-26 04:01:19 +0530

    Android 13

--- beryllium.sh
+++ beryllium.sh
@@ -1,6 +1,7 @@
-git clone https://github.com/aghora7/device_xiaomi_beryllium device/xiaomi/beryllium
-git clone https://github.com/Aghora7/device_xiaomi_sdm845-common device/xiaomi/sdm845-common
-git clone https://github.com/PainKiller3/kernel_xiaomi_sdm845 kernel/xiaomi/sdm845
-git clone https://github.com/PainKiller3/vendor_xiaomi vendor/xiaomi
+git clone https://github.com/RahulGorai0206/device_xiaomi_beryllium -b thirteen device/xiaomi/beryllium
+git clone https://github.com/RahulGorai0206/device_xiaomi_sdm845-common.git -b thirteen device/xiaomi/sdm845-common
+git clone https://github.com/RahulGorai0206/vendor_xiaomi -b thirteen vendor/xiaomi
+git clone https://github.com/PainKiller3/kernel_xiaomi_sdm845.git -b thirteen kernel/xiaomi/sdm845
+git clone https://gitlab.com/RahulGorai0206/firmware_xiaomi_beryllium -b 13 firmware/xiaomi/beryllium
 git clone https://github.com/lineageos/android\_packages\_resources\_devicesettings packages/resources/devicesettings
 git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-custom --depth=1
