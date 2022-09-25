c17cecd3da7870217dbd8f237021962f042caa68
Aghora7 <ukikieran@gmail.com>
2022-09-26 04:22:56 +0530

    Update beryllium.sh

--- beryllium.sh
+++ beryllium.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/RahulGorai0206/device_xiaomi_beryllium -b 13-main device/xiaomi/beryllium
 git clone https://github.com/RahulGorai0206/device_xiaomi_sdm845-common.git -b thirteen device/xiaomi/sdm845-common
-git clone https://github.com/RahulGorai0206/vendor_xiaomi -b thirteen vendor/xiaomi
+git clone https://github.com/RahulGorai0206/vendor_xiaomi -b 13 vendor/xiaomi
 git clone https://github.com/PainKiller3/kernel_xiaomi_sdm845.git -b thirteen kernel/xiaomi/sdm845
 git clone https://gitlab.com/RahulGorai0206/firmware_xiaomi_beryllium -b 13 firmware/xiaomi/beryllium
 git clone https://github.com/lineageos/android_packages_resources_devicesettings -b lineage-20.0 packages/resources/devicesettings
